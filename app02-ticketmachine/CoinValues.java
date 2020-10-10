1ffa5cc52daad68a180786f0c8b3fa6b9af7badd
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2020-10-10 19:57:09 +0100

    changed enum from coin into coinValues

---
+++ app02-ticketmachine/CoinValues.java
@@ -0,0 +1,30 @@
+
+/**
+ * Enumeration class Coin - write a description of the enum class here
+ *
+ * @author Kate Gordon
+ * @version 09/10/2020
+ */
+
+public enum CoinValues
+{
+    P10 (10),
+    P20 (20),
+    P100 (100),
+    P200 (200);
+    
+    private final int value;
+    
+    private CoinValues(int value)
+    
+    {
+        this.value = value;
+    }
+    
+    public int getValue()
+    {
+        return value;
+    }
+    
+   
+}
