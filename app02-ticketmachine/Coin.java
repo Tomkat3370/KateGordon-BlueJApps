dbd6f4998d32585c9a8245fa90574b8cda4d43c9
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2020-10-10 20:22:34 +0100

    constructor built
    needs adjustments to finish

--- app02-ticketmachine/Coin.java
+++ app02-ticketmachine/Coin.java
@@ -7,18 +7,30 @@
  */
 public class Coin
 {
-   // the width of the coin in milimeters(mm)
+    // the width of the coin in milimeters(mm)
     private int width;
     
     // the weight of the coin in miligrams (mg)
     private int weight;
-   
+    
+    // the value of the coin based on available coin values
+    private  CoinValues coinValue;
+  
+      
+    /**
+     * Constructor for objects of class Coin
+     */
+    public Coin(CoinValues coinValue)
+    {
+       this.coinValue = coinValue;
+    }
     
     /**
      * Constructor for objects of class Coin
      */
-    public Coin()
+    public Coin(CoinValues coinValue, int width, int weight)
     {
-        
+        this.width = width;
+        this. weight = weight;
     }
 }
