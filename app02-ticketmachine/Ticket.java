537552c895b06691ffb14e9d26e61382baaef9d8
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2020-10-15 16:58:50 +0100

    returned Date as String

--- app02-ticketmachine/Ticket.java
+++ app02-ticketmachine/Ticket.java
@@ -1,5 +1,5 @@
 import java.util.Date;
-
+import java.text.SimpleDateFormat;
 /**
  * Write a description of class Ticket here.
  *
@@ -9,12 +9,16 @@ import java.util.Date;
 public class Ticket
 {
     // Attributes
-    
     private String destination;
-    private int price;
-    private Date purchasedDateTime;
     
-        /**
+    private int price;
+    
+    private Date today = new Date();
+    
+    private SimpleDateFormat formatter = 
+            new SimpleDateFormat("dd/MMM/yyy");
+    
+    /**
      * Constructor for objects of class Ticket setting
      * the destination and price. The date will
      * contain the current system date and time
@@ -22,17 +26,26 @@ public class Ticket
     public Ticket(String destination, int price)
     {
         this.destination = destination;
-        this.price = price;
         
-        purchasedDateTime = new Date();
-    }
+        this.price = price;
+                     
+        today = new Date();
+     }
     
     /**
+     * Get the Date for the current Ticket
+     */
+    public String getDate()
+    {
+       return formatter.format(today); 
+    }
+
+     /**
      * Print the ticket to include destination, price and 
      */
     public void print()
     {
         System.out.println(" Ticket " + destination +
-            " Price " + price + " Purchased " + purchasedDateTime);
+            " Price " + price + " Purchased " + today);
     }
 }   
