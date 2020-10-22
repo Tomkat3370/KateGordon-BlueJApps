9b22c7af7307751aa83da8c3e03fa4ccb7f09d4c
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2020-10-22 22:04:44 +0100

    Create Ticket.java

---
+++ app02-ticketmachine-advanced/Ticket.java
@@ -0,0 +1,74 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+/**
+ * Ticket class will store information about a ticket
+ * including the journey destination, price and the
+ * date and time the ticket was purchased
+ *
+ * @author Kate Gordon
+ * @version 09/01/2020
+ */
+public class Ticket
+{
+    // Attributes
+    private String destination;
+    
+    // currency cents
+    private int price;
+    
+    private Date today = new Date();
+    
+    private SimpleDateFormat formatter = 
+            new SimpleDateFormat("dd/MMM/yyy");
+    
+    /**
+     * Constructor for objects of class Ticket setting
+     * the destination and price. The date will
+     * contain the current system date and time
+     */
+    public Ticket(String destination, int price)
+    {
+        this.destination = destination;
+        
+        this.price = price;
+                     
+        today = new Date();
+        
+    }
+    
+    /**
+     * Get the Price for the current Ticket
+     */
+    public int getPrice()
+    {
+        return price;
+    }
+    
+    /**
+     * Get the destination for the current Ticket
+     */
+    public String getDestination()
+    {
+        return destination;
+    }
+    
+    /**
+     * Get the Date for the current Ticket
+     */
+    public String getDate()
+    {
+       return formatter.format(today); 
+    }
+
+    /**
+    * Print the ticket to include destination, price and 
+    */
+    public void print()
+    {
+        System.out.println();
+        System.out.println(" Ticket to " + destination );
+        System.out.println(" " + price + " cents ");
+        System.out.println( " Purchased " + today);
+        System.out.println();
+    }
+}   
