2ae40f7b5bbf543e6dcc09c160deee183da02e7c
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2020-10-22 22:05:08 +0100

    Create Module.java

---
+++ app03-studentGrades/Module.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class Module here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Module
+{
+    //Attributes
+    private String title;
+    
+    private String code;
+    
+    private int mark;
+    
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module(String title, String codeNo)
+    {
+        this.title = title;
+        this.codeNo = codeNo;
+        mark = 0;
+    }
+    
+    /**
+     * 
+     */
+    public void
+}
