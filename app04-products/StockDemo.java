599f79917f17034178bf09db66b5443875d4bdb4
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2020-11-01 22:03:40 +0000

    products added

--- app04-products/StockDemo.java
+++ app04-products/StockDemo.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /**
  * Demonstrate the StockManager and Product classes.
  * The demonstration becomes properly functional as
@@ -5,6 +6,8 @@
  * 
  * @author David J. Barnes and Michael Kölling.
  * @version 2016.02.29
+ * @modified by Kate Gordon
+ * @dated 01/11/2020
  */
 public class StockDemo
 {
@@ -19,9 +22,16 @@ public class StockDemo
     {
         this.manager = manager;
         //choose products with full name for easy searching and create 10
-        manager.addProduct(new Product(132, "Clock Radio"));
-        manager.addProduct(new Product(37,  "Mobile Phone"));
-        manager.addProduct(new Product(23,  "Microwave Oven"));
+        manager.addProduct(new Product(100, "Stradivarous Violin"));
+        manager.addProduct(new Product(101, "Casio Keyboard"));
+        manager.addProduct(new Product(102, "Alexander FrenchHorn"));
+        manager.addProduct(new Product(103, "Music Stand"));
+        manager.addProduct(new Product(104, "Violin Strings"));
+        manager.addProduct(new Product(105, "Guitar Strings"));
+        manager.addProduct(new Product(106, "FrenchHorn Mute"));
+        manager.addProduct(new Product(107, "Violin Resin"));
+        manager.addProduct(new Product(108, "Violin Stand"));
+        manager.addProduct(new Product(109, "Guitar Stand"));
     }
     
     /**
@@ -33,12 +43,38 @@ public class StockDemo
     {
         // Show details of all of the products.
         manager.printAllProduct();
-        // Take delivery of 5 items of one of the products.
-        manager.delivery(132, 5);
-        // add 9 more deliveries
+        
+        manager.delivery(100, 1);
+        manager.delivery(101, 3);
+        manager.delivery(102, 2);
+        manager.delivery(103, 5);
+        manager.delivery(104, 10);
+        manager.delivery(105, 5);
+        manager.delivery(106, 3);
+        manager.delivery(107, 12);
+        manager.delivery(108, 4);
+        manager.delivery(109, 4);
+        
         manager.printAllProduct();
     }
     
+     /**
+     * Get the product with the given id from the manager.
+     * An error message is printed if there is no match.
+     * @param id The ID of the product.
+     * @return The Product, or null if no matching one is found.
+     */
+    public Product getProduct(int id)
+    {
+        Product product = manager.findProduct(id);
+
+        if(product == null) 
+        {
+            System.out.println("Product with ID: " + id +
+                " is not recognised.");
+        }
+        return product;
+    }
 
     /**
      * @return The stock manager.
