078275f386275a08a60554b1b0e47f7c7ce8bef7
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2020-11-27 00:06:43 +0000

    Create ProductList.java

---
+++ app05-stock/ProductList.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class ProductList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public final class ProductList
+{
+        public static final String Product_FIRST = "Stradivarous Violin";
+        public static final String Product_SECOND = "Casio Keyboard";
+        public static final String Product_THIRD = "Alexander FrenchHorn";
+        public static final String Product_FOURTH = "Music Stand";
+        public static final String Product_FIFTH = "Violin Strings";
+        public static final String Product_SIXTH = "Guitar Strings";
+        public static final String Product_SEVENTH = "FrenchHorn Mute";
+        public static final String Product_EIGHTH = "Violin Resin";
+        public static final String Product_NINTH = "Violin Stand";
+        public static final String Product_TENTH = "Guitar Stand";
+}
